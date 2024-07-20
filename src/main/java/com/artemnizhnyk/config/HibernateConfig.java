cc2deaa57e6a920c6f794f8f5a55d4fc8cff13db
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-07-20 21:41:03 +0200

    some test changes were added

--- src/main/java/com/artemnizhnyk/config/HibernateConfig.java
+++ src/main/java/com/artemnizhnyk/config/HibernateConfig.java
@@ -4,20 +4,20 @@ import org.hibernate.Session;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateConfig {
-    private Configuration configuration;
-
-    private void configure() {
-        configuration = new Configuration();
-        configuration.configure();
-        }
-
-    public Session getSession() {
-        if (configuration == null) {
-            configure();
-        }
-        try (var sessionFactory = configuration.buildSessionFactory();
-        var session = sessionFactory.openSession()){
-            return session;
-        }
-    }
+//    private Configuration configuration;
+//
+//    private void configure() {
+//        configuration = new Configuration();
+//        configuration.configure();
+//        }
+//
+//    public Session getSession() {
+//        if (configuration == null) {
+//            configure();
+//        }
+//        try (var sessionFactory = configuration.;
+//        var session = sessionFactory.openSession()){
+//            return session;
+//        }
+//    }
     }
--- src/main/java/com/artemnizhnyk/tasklistjakarta/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/artemnizhnyk/tasklistjakarta/repository/impl/UserRepositoryImpl.java
@@ -4,27 +4,35 @@ import com.artemnizhnyk.config.HibernateConfig;
 import com.artemnizhnyk.tasklistjakarta.domain.model.Task;
 import com.artemnizhnyk.tasklistjakarta.domain.model.User;
 import com.artemnizhnyk.tasklistjakarta.repository.UserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class UserRepositoryImpl implements UserRepository {
 
+    @PersistenceContext
+    private EntityManager entityManager;
 //    private final HibernateConfig hibernateConfig = new HibernateConfig();
 
     @Override
     public User getUserById(Long id) {
-        Configuration configuration = new Configuration();
-        configuration.configure();
-        configuration.addAnnotatedClass(User.class);
-        configuration.addAnnotatedClass(Task.class);
-        Session session;
-        try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
-            session = sessionFactory.openSession();
-            session.beginTransaction();
-            User user = session.get(User.class, id);
-            session.getTransaction().commit();
-            return user;
-        }
+//        Configuration configuration = new Configuration();
+//        configuration.configure();
+//        configuration.addAnnotatedClass(User.class);
+//        configuration.addAnnotatedClass(Task.class);
+//        Session session;
+//        try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
+//            session = sessionFactory.openSession();
+//            session.beginTransaction();
+//            User user = session.get(User.class, id);
+//            session.getTransaction().commit();
+//            return user;
+//        }
+        entityManager.getTransaction().begin();
+        User user = entityManager.find(User.class, 1);
+        entityManager.getTransaction().commit();
+        return user;
     }
 }
